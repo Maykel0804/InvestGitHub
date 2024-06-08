47db6ac501c4ec69f0f17847a1c2989d330f6e2c
Maykel0804 <maykel0804@hotmail.com>
2024-06-07 19:03:55 -0600

    ejemplo de uso de Git en netbeans

---
+++ src/main/java/com/mycompany/github/GitHub.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.github;
+
+/**
+ *
+ * @author mayke
+ */
+public class GitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
